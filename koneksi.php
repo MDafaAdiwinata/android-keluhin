23828dfda548b2b34a0be19a41331780f476c506
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-06-15 07:19:24 +0700

    add Dockerfile dan update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,11 @@
 <?php
-$server = "localhost";
-$user = "root";
-$pass = "";
-$db = "keluhin";
+$host = getenv('DB_HOST')     ?: 'localhost';
+$user = getenv('DB_USER')     ?: 'root';
+$pass = getenv('DB_PASS')     ?: '';
+$db   = getenv('DB_NAME')     ?: 'nama_db_kamu';
+$port = getenv('DB_PORT')     ?: 3306;
 
-$koneksi = mysqli_connect($server, $user, $pass, $db);
+$koneksi = new mysqli($host, $user, $pass, $db, (int)$port);
 
 if (!$koneksi) {
     die("Koneksi Gagal: " . mysqli_connect_error());
